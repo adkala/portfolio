302ac5e627fc27ededd5d6fd9f1bcfd560b61708
ADDISON KALANTHER <addikala@berkeley.edu>
2022-07-21 20:30:56 -0700

    removed .cache

--- .cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-tsx": () => import("./../../../src/pages/404.tsx" /* webpackChunkName: "component---src-pages-404-tsx" */),
-  "component---src-pages-art-art-page-tsx": () => import("./../../../src/pages/art/{art.page}.tsx" /* webpackChunkName: "component---src-pages-art-art-page-tsx" */),
-  "component---src-pages-art-tsx": () => import("./../../../src/pages/art.tsx" /* webpackChunkName: "component---src-pages-art-tsx" */),
-  "component---src-pages-index-tsx": () => import("./../../../src/pages/index.tsx" /* webpackChunkName: "component---src-pages-index-tsx" */),
-  "component---src-pages-tech-tsx": () => import("./../../../src/pages/tech.tsx" /* webpackChunkName: "component---src-pages-tech-tsx" */)
-}
-
