c51d801a784e2184a956a34e0334c79a7bdd5d57
ADDISON KALANTHER <addikala@berkeley.edu>
2022-09-26 02:43:05 -0700

    removed unnecessary files

--- src/components/aside/AsideDesktop.tsx
+++
@@ -1,8 +0,0 @@
-import Aside from "./Aside";
-import React from "react";
-
-const AsideDesktop: React.FC = () => {
-  return <Aside desktop={true} />;
-};
-
-export default AsideDesktop;
--- src/components/aside/AsideMobile.tsx
+++
@@ -1,8 +0,0 @@
-import Aside from "./Aside";
-import React from "react";
-
-const AsideMobile: React.FC = () => {
-  return <Aside desktop={false} />;
-};
-
-export default AsideMobile;
